95dc6b637bae93653ff8d10a6c1e52e5a0399b08
dmclink <135415819+dmclink@users.noreply.github.com>
2025-08-19 12:30:16 +0800

    feat: add ui elements, styles, controls

---
+++ src/js/etch.js
@@ -0,0 +1,21 @@
+const initSize = 16;
+
+const screen = document.querySelector('.screen');
+
+/** Creates new div to represent "pixel" element and attaches it to screen.
+ *
+ * @param {HTMLElement} screen - the container element which new pixels are attached
+ */
+function createPixel(screen, numPixels) {
+	var pixel = document.createElement('div');
+
+	pixel.classList.add('pixel');
+
+	pixel.style.flexGrow = 1;
+	pixel.style.flexBasis = (1 / numPixels) * 100 + '%';
+	screen.appendChild(pixel);
+}
+
+for (let i = 0; i < initSize * initSize; i++) {
+	createPixel(screen, initSize);
+}
